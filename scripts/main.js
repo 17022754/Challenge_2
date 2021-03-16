9459b5422bf6c5edda1e61a040e47ddc8d3366e8
17022754 <17022754@student.hhs.nl>
2021-03-16 13:10:20 +0100

    Added images and style
    Added the images for the different times of the day. Added the javascript code to make the images appear at the right time. Added the styles to make it visually appealing.

--- scripts/main.js
+++ scripts/main.js
@@ -19,21 +19,30 @@ function showTime(){
 
     setTimeout(showTime, 1000);
     
-}
+};
 
 showTime();
 
-function differentBackground(){
+function addElements(){
   var hr = (new Date()).getHours();
+  var images = ['images/marsmannetje-day.png', 'images/marsmannetje-night.png'];
+  var text = ['Op aarde is het nu na 11.00 uur, dus tijd om in beweging te komen! Tip: Door te sporten kom je sneller over een jetlag heen én het is gezond voor je, dus dubbele winst!', 
+              'Goedeavond, hopelijk heb je een fijne dag gehad. Als je nog niet hebt gegeten, moet je dat snel doen! Het is namelijk al bijna tijd om naar bed te gaan. Tip: Volg hetzelfde slaapritme als op Aarde, zo blijf je in je ritme.']
 
-  if(hr > 6 && hr < 9){
+  if(hr > 6 && hr < 11){
     document.getElementsByTagName("body")[0].style.backgroundImage = "url('images/background-morning.jpg')";
-  } else if (hr > 9 && hr < 20){
+  } else if (hr > 11 && hr < 20){
     document.getElementsByTagName("body")[0].style.backgroundImage = "url('images/background-day.png')";
+    document.getElementById("images").src = images[0];
+    document.getElementById("text").textContent = text[0];
+    document.getElementById("time").style.color = 'rgb(31, 41, 64)';
   } else {
     document.getElementsByTagName("body")[0].style.backgroundImage = "url('images/background-night.jpeg')";
+    document.getElementById("images").src = images[1];
+    document.getElementById("text").textContent = text[1];
+    document.getElementsByTagName("h1")[0].style.color = 'rgb(255, 255, 255)';
   }
-}
+};
 
-setInterval(differentBackground, 1000*60*60); /*Een keer in het uur testen of de achtergrond nog juist is.*/
-differentBackground();
+setInterval(addElements, 1000*60*60); /*Een keer in het uur testen of de achtergrond nog juist is.*/
+addElements();
